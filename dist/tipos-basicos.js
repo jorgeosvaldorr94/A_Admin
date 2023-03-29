872e55002c8aafc7a16b81f41a8a629478470c05
Jorge Osvaldo Redonda Rodríguez <jorgeosvaldorr94@gmail.com>
2023-03-29 00:24:13 -0300

    Testing Class in TypeScript

---
+++ dist/tipos-basicos.js
@@ -0,0 +1,11 @@
+"use strict";
+let nombre = "Jorge";
+nombre = 'Osvaldo';
+let skills = `
+    Angular
+    React
+    Git
+    Node
+`;
+console.log(`Mi nombre es: ${nombre}`);
+console.log(`Mis habilidades son: ${skills}`);
---
+++ src/tipos-basicos/tipos-basicos.ts
@@ -0,0 +1,13 @@
+let nombre:string = "Jorge";
+
+nombre = 'Osvaldo';
+
+let skills:string = `
+    Angular
+    React
+    Git
+    Node
+`;
+
+console.log(`Mi nombre es: ${nombre}`);
+console.log(`Mis habilidades son: ${skills}`);
