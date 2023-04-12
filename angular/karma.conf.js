72f5415216cb8bf75a7d3b663fdfdb156c040908
Jorge Osvaldo Redonda Rodríguez <jorgeosvaldorr94@gmail.com>
2023-04-12 04:07:45 -0300

    Configuring File

---
+++ angular/karma.conf.js
@@ -0,0 +1,39 @@
+// karma.conf.js
+module.exports = function(config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        jasmine: {
+          // you can add configuration options for Jasmine here
+          // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+          failFast: true,
+          timeoutInterval: 10000
+        },
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage/A_Admin'),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' }
+        ]
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false,
+      restartOnFileChange: true
+    });
+  };
